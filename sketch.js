c1ddf45c10e8b603e54593c90450fd7d6955719a
Pranjul-7 <77836111+Pranjul-7@users.noreply.github.com>
2021-05-01 15:24:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,108 @@
+const Engine = Matter.Engine,
+      World = Matter.World,
+      Events = Matter.Events,
+      Bodies = Matter.Bodies
+
+var engine,world; 
+var particles = [];
+var plinkos=[];
+var rows=[];
+var ground;
+var rows;
+var backgroundImg,bg;
+
+var rowsHeight = 150;
+
+function preload() {
+  backgroundImg = loadImage("light.jpg");
+}
+
+
+function setup() {
+
+  
+  createCanvas(600,500);
+  
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,500,1700,10);
+
+  for(var j = 40; j<= width; j = j+50){
+    plinkos.push(new Plinko(j , 55))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,85))
+  }
+  for(var j = 40; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,120))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,150))
+  }
+  for(var j = 40; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,185))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,225))
+  }
+  for(var j = 40; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,265))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,305))
+  }
+  for(var k = 0; k <=width; k = k+80){
+    rows.push(new Rows(k, height-rowsHeight/2, 10, rowsHeight));
+  }
+}
+
+function draw() {
+
+  if(backgroundImg){
+    background(backgroundImg);}
+
+  if(frameCount % 30 === 0){
+    particle = new Particle(random(130, 400), 0, 7, random(0, 360));
+    particles.push(particle);
+  }
+
+  
+  Engine.update(engine, 25);
+  for(var i = 0; i < particles.length; i++){
+    particles[i].display();
+  }
+
+ 
+  for(var k = 0; k<rows.length;k++){
+    rows[k].display();
+ }
+
+
+  for(var j = 0; j<plinkos.length;j++){
+    plinkos[j].display();
+ }
+
+
+//rows.display();
+ ground.display();
+
+  drawSprites();
+}
+async function getTime(){ 
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata"); 
+  var responseJSON = await response.json(); 
+  console.log(responseJSON); 
+  var datetime = responseJSON.datetime;
+  console.log(datetime);
+  var hour =datetime.slice(11,13);
+  if(hour>=06 && hour<=19)
+  { 
+  bg = "light.jpg"; 
+  } 
+  else
+  {
+  bg = "dark.jpg";
+  }
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
+}
